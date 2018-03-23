8cd3e7af4fea8602053228f815ce336dd72ff097
sauloquinteiro@gmail.com <sauloquinteiro@gmail.com>
2018-03-23 18:43:36 -0500

    Stage 1 and 2 done

--- main.c
+++ main.c
@@ -14,16 +14,16 @@
 /* Constants */
 
 #define NWallet 1 // number of Wallets
-#define TR 100 // number of transactions
-#define Balance 100
+#define TR 20 // number of transactions
+#define Balance 5
 #define PRINT_VECS 1
-#define DEBUG 0
+#define DEBUG 1
 #define MAX_RAND 10000  // max value of elements generated for array
 
 
 /* Prototypes */
-void print_vec(const char *label, double *vec);
-void print_mat(const char *label, double *matrix);
+void print_vec(const char *label, double vec[NWallet]);
+void print_mat(const char *label, double matrix[NWallet][TR]);
 void initialization(double vec[], int size);
 float random();
 int main(int argc, char *argv[]){
@@ -34,33 +34,53 @@ int main(int argc, char *argv[]){
 	double transactions[NWallet][TR];
 	double amount; 
 	initialization(wallets, NWallet);
-	for(int i = 0; i < TR ; i++){
-		amount = random();
-		
-		if(amount <  wallets[0])
-		{
-			wallets[0] -= amount;
-			transactions[0][i] = -amount;
-		}else
-		{
-			transactions[0][i] = 0;			
+	print_vec("Initial Balance", wallets);
+	for(int row = 0; row < NWallet ; ++row)
+		for(int i = 0; i < TR ; i++){
+			amount = random();
+			
+			if(amount <  wallets[row])
+			{
+				wallets[row] -= amount;
+				transactions[row][i] = -amount;
+				#if DEBUG
+					printf("Transaction of %f - APPROVED\n", amount);
+				#endif
+			}else
+			{
+				transactions[row][i] = 0;
+				#if DEBUG
+					printf("Transaction of %f - REFUSED\n", amount);
+				#endif
+			}
 		}
-	}
+	print_vec("Remain Balance", wallets);
+	print_mat("Transactions Table", transactions);
 	random();
 	
 	
 	return EXIT_SUCCESS;;
 }
 
-void print_vec(const char *label, double *vec){
+void print_vec(const char *label, double vec[NWallet]){
 	printf("%s\n", label);
 	for(int i =0; i<NWallet;++i)
 		printf("%.2f\t", vec[i]);
+	printf("\n\n");
 	return;
 }
-void print_mat(const char *label, double *matrix){
-}
 
+void print_mat(const char *label, double matrix[NWallet][TR]){
+	printf("%s\n", label);
+	for(int i =0; i<NWallet;++i){
+		printf("L%i - \t", i);
+		for(int j = 0; j< TR; ++j)
+			printf("(%.2f)\t", matrix[i][j]);
+		printf("\n");
+	}
+	printf("\n\n");
+	return;
+}
 // Initializa the vector with minimal amount
 void initialization(double vec[], int size){
 	for(int i =0 ; i< size; i++){
@@ -70,19 +90,9 @@ void initialization(double vec[], int size){
 
 
 float random (){
-	double random ;
-	int attempt = 0;
+	double random ;	
 	do{
 		random = rand() % MAX_RAND;	
-		#if DEBUG
-		attempt++;
-		#endif
 	}while(random == 0.0);
-
-	#if DEBUG
-		if (attempt > 1)
-			printf("Attempt = %i\n", attempt);
-		printf("Result = %f\n\n", random / MAX_RAND);
-	#endif
 	return (random) / MAX_RAND;
 	}
